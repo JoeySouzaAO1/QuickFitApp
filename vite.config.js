f00b56363257ac0ccb3e31c79654d0ce59126577
Joey Souza <Souzajoey@gmail.com>
2025-03-03 17:52:28 +0000

    Trying to figure out Deployment isues

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/QuickFitApp/'
+  base: '/QuickFitApp/',
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    },
+  },
 })
